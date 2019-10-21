19558325ab320ca50720590f9dc5e0ea1b37d4df
Kay Zin <zskay@timesprinters.com>
2019-10-21 16:16:12 +0800

    Get user location

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -30,6 +30,13 @@ class ViewController extends Controller
             return view('index',compact('place','jobs'));
         }
 
+        $PublicIP = get_client_ip();
+        $json     = file_get_contents("http://ipinfo.io/$PublicIP/geo");
+        $json     = json_decode($json, true);
+        $country  = $json['country'];
+        $region   = $json['region'];
+        $city     = $json['city'];
+
     }
 
     public function thankyou(Request $request){
@@ -53,6 +60,26 @@ class ViewController extends Controller
         }
         return Redirect::intended($url);
 
-
     }
+
+    function get_client_ip(){
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        } else if (isset($_SERVER['HTTP_FORWARDED'])) {
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        } else if (isset($_SERVER['REMOTE_ADDR'])) {
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        } else {
+            $ipaddress = 'UNKNOWN';
+        }
+
+        return $ipaddress;
+      }
 }
