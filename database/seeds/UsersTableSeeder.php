db9a466a3a44a1897bef65574c8b291a961e6756
Kay Zin <zskay@timesprinters.com>
2019-11-04 17:14:45 +0800

    Add user seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+        ['name' => 'Admin', 'email' => 'admin@gmail.com','role_id'=> '1'],
+        ]);
+    }
+}
