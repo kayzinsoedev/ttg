bc84f499f9e04e6b7ab6514d8f0da5022a4cc2b5
Kay Zin <zskay@timesprinters.com>
2019-11-04 17:48:15 +0800

    role id nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('role_id');
+            $table->string('role_id')->nullable();
             $table->string('password');
             $table->softDeletes();
             $table->rememberToken();
